523b22aa350b26939ba18425d62766cbf12af5ba
MChlopeckii <90689684+MChlopeckii@users.noreply.github.com>
2021-11-09 13:30:03 +0100

    Loot boxes window

--- openWindow.js
+++ openWindow.js
@@ -1,13 +1,24 @@
 let cardWindowOpen = false;
+let lootBoxOpen = false;
 function openCardsWindow() {
-    if (cardWindowOpen == false) {
-        document.getElementById("cards").style.height = 630 + "px";
-        document.getElementById("cardsWindow").style.height = 700 + "px";
-        cardWindowOpen = true;
+    let window = document.getElementById("cardsWindow");
+    let field = document.getElementById("cards");
+    cardWindowOpen = openingHandler(window, field, cardWindowOpen);
+}
+function openLootWindow() {
+    let window = document.getElementById("lootWindow");
+    let field = document.getElementById("loot");
+    lootBoxOpen = openingHandler(window, field, lootBoxOpen);
+}
+function openingHandler(field, window, checker) {
+    if (checker == false) {
+        field.className = "openWindow file";
+        window.classList = "openField";
+        return true;
     }
     else {
-        document.getElementById("cards").style.height = 0 + "px";
-        document.getElementById("cardsWindow").style.height = 70 + "px";
-        cardWindowOpen = false;
+        field.className = "file";
+        window.className = "closedField";
+        return false;
     }
 }
