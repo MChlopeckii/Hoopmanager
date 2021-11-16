37312a1b51e2d36d62e8215e7ef930fe9d814e56
MChlopeckii <90689684+MChlopeckii@users.noreply.github.com>
2021-11-16 12:55:37 +0100

    all windows opening and closing

--- openWindow.js
+++ openWindow.js
@@ -1,24 +1,39 @@
 let cardWindowOpen = false;
 let lootBoxOpen = false;
+let playsWindowOpen = false;
+let simWindowOpen = false;
+
 function openCardsWindow() {
-    let window = document.getElementById("cardsWindow");
-    let field = document.getElementById("cards");
+    const window = document.getElementById("cardsWindow");
+    const field = document.getElementById("cards");
     cardWindowOpen = openingHandler(window, field, cardWindowOpen);
 }
 function openLootWindow() {
-    let window = document.getElementById("lootWindow");
-    let field = document.getElementById("loot");
+    const window = document.getElementById("lootWindow");
+    const field = document.getElementById("loot");
     lootBoxOpen = openingHandler(window, field, lootBoxOpen);
+}
+function openPlaysWindow() {
+    const window = document.getElementById("playsWindow");
+    const field = document.getElementById("plays");
+    playsWindowOpen = openingHandler(window, field, playsWindowOpen);
+}
+function openSimWindow() {
+    const window = document.getElementById("simWindow");
+    const field = document.getElementById("sim");
+    simWindowOpen = openingHandler(window, field, simWindowOpen);
 }
 function openingHandler(field, window, checker) {
     if (checker == false) {
         field.className = "openWindow file";
         window.classList = "openField";
+        window.style.height = "630px";
         return true;
     }
     else {
         field.className = "file";
         window.className = "closedField";
+        setTimeout(() => { window.style.height = "0px" }, 500);
         return false;
     }
 }
